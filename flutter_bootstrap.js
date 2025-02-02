65cb1522219e11ff97ba177914d3e6e2a41a464b
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2025-02-02 20:20:20 +0530

    Builded

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4169088042"
+    serviceWorkerVersion: "3218908980"
   }
 });
